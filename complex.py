91636efd7f5f51e28d0c23235643691f595a885a
ryancirish <organized.ryancirish@gmail.com>
2019-09-23 21:34:54 -0400

    f1rst commit

---
+++ complex.py
@@ -0,0 +1,53 @@
+import math
+
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+
+    def __add__(self, no):
+        real = self.real + no.real
+        img = self.imaginary + no.imaginary
+        return Complex(real, img)        
+        
+    def __sub__(self, no):
+        real = self.real - no.real
+        img = self.imaginary - no.imaginary
+        return Complex(real, img)
+
+    def __mul__(self, no):
+        real = self.real * no.real - self.imaginary * no.imaginary
+        img = self.real * no.imaginary + self.imaginary * no.real
+        return Complex(real, img)
+
+    def __truediv__(self, no):
+        num1 = self.real * no.real - self.imaginary * no.imaginary
+        num2 = self.real * no.imaginary + self.imaginary * no.real
+        denominator = no.real ** 2 + no.imaginary ** 2
+        real = num1 / denominator
+        img = num2 / denominator
+        return Complex(real, img)
+
+    def mod(self):
+        return Complex(math.sqrt(self.real ** 2 + self.imaginary ** 2), 0)
+
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
